36a7c43d68489af003fbdc71ec5f2f419e5f0378
dharitriratna <dharitri@ratnatechnology.com>
2018-11-05 17:52:56 +0530

    dharitri_change

--- Genie/app/src/main/java/com/example/user/genie/Fragments/ShowTimeFragment.java
+++ Genie/app/src/main/java/com/example/user/genie/Fragments/ShowTimeFragment.java
@@ -51,11 +51,11 @@ public class ShowTimeFragment extends Fragment implements View.OnClickListener {
         View v= inflater.inflate(R.layout.fragment_show_time, container, false);
         RecyclerView cinemahallRecyclerview=(RecyclerView)v.findViewById(R.id.cinemahallRecyclerview);
          todayTv=(TextView)v.findViewById(R.id.todayTv);
-         /*tommorwTv=(TextView)v.findViewById(R.id.tommorwTv);
-         thirdTv=(TextView)v.findViewById(R.id.thirdTv);*/
+         tommorwTv=(TextView)v.findViewById(R.id.tommorwTv);
+         thirdTv=(TextView)v.findViewById(R.id.thirdTv);
         todayTv.setOnClickListener(this);
-       /* tommorwTv.setOnClickListener(this);
-        thirdTv.setOnClickListener(this);*/
+        tommorwTv.setOnClickListener(this);
+        thirdTv.setOnClickListener(this);
 
 
         cinemahallRecyclerview.setHasFixedSize(true);
@@ -88,7 +88,7 @@ public class ShowTimeFragment extends Fragment implements View.OnClickListener {
         String sec=secarry[0];
         String sec2=secarry[1];
         String tomorrow_date=sec+" "+sec2;
-   //     tommorwTv.setText(tomorrow_date);
+        tommorwTv.setText(tomorrow_date);
         //check_tw_date=tomorrow;
 
         String th=getNextDate(tomorrow);
@@ -103,7 +103,7 @@ public class ShowTimeFragment extends Fragment implements View.OnClickListener {
         String third1=thirdarry[0];
         String third2=thirdarry[1];
         String third=third1+" "+third2;
-      //  thirdTv.setText(third);
+        thirdTv.setText(third);
 
         String fourth=getNextDate(th);
         Log.d("TAG","fourth==>"+fourth);
@@ -141,35 +141,33 @@ public class ShowTimeFragment extends Fragment implements View.OnClickListener {
             case R.id.todayTv:
                 todayTv.setTextColor(getResources().getColor(R.color.colorPrimary));
                 todayTv.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-                /*thirdTv.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                thirdTv.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
                 thirdTv.setBackgroundResource(R.drawable.edittext_top_bg);
                 tommorwTv.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
                 tommorwTv.setBackgroundResource(R.drawable.edittext_top_bg);
 
-*/
+
                 break;
-         /*   case R.id.thirdTv:
+            case R.id.thirdTv:
                 thirdTv.setTextColor(getResources().getColor(R.color.colorPrimary));
                 thirdTv.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
                 todayTv.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
                 todayTv.setBackgroundResource(R.drawable.edittext_top_bg);
                 tommorwTv.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
                 tommorwTv.setBackgroundResource(R.drawable.edittext_top_bg);
-                break;*/
 
 
-
-          /*  case R.id.tommorwTv:
+                break;
+            case R.id.tommorwTv:
                 tommorwTv.setTextColor(getResources().getColor(R.color.colorPrimary));
                 tommorwTv.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
                 todayTv.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
                 todayTv.setBackgroundResource(R.drawable.edittext_top_bg);
                 thirdTv.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
                 thirdTv.setBackgroundResource(R.drawable.edittext_top_bg);
-                 break;*/
 
 
-
+                break;
         }
     }
 }
