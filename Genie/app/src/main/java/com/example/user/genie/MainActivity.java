1b6595812f28326abf483699d3bab5e5c5797d05
dharitriratna <dharitri@ratnatechnology.com>
2018-11-30 17:12:46 +0530

    dharitri_30_11

--- Genie/app/src/main/java/com/example/user/genie/MainActivity.java
+++ Genie/app/src/main/java/com/example/user/genie/MainActivity.java
@@ -317,7 +317,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         rawMeat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,RawMeatActivity.class));
+                startActivity(new Intent(MainActivity.this,ComingSoonActivity.class));
             }
         });
 
