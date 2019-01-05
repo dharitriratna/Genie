3bfcf557cd31e6907ef00218b1fae6c46a720b03
dharitriratna <dharitri@ratnatechnology.com>
2019-01-05 15:13:04 +0530

    subham_jan_5

--- Genie/app/src/main/java/ratna/genie1/user/genie/PayForElectricity.java
+++ Genie/app/src/main/java/ratna/genie1/user/genie/PayForElectricity.java
@@ -60,6 +60,7 @@ public class PayForElectricity extends AppCompatActivity {
     String login_user="";
 
     String statename;
+    String groupId;
 
 
     @Override
@@ -68,11 +69,26 @@ public class PayForElectricity extends AppCompatActivity {
         setContentView(R.layout.activity_pay_for_electricity);
         toolbar = findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24dp);
+        groupId = RegPrefManager.getInstance(getApplicationContext()).getUserGroup();
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                finish();
+                if (groupId.equals("4")){
+                    startActivity(new Intent(getApplicationContext(),MainActivity2.class));
+                    finish();
+                }
+                else if (groupId.equals("5")){
+                    startActivity(new Intent(getApplicationContext(),MainActivity3.class));
+                    finish();
+                }
+                else if (groupId.equals("3")){
+                    startActivity(new Intent(getApplicationContext(),MainActivity4.class));
+                    finish();
+                }
+                else if (groupId.equals("2")){
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                }
             }
         });
 
